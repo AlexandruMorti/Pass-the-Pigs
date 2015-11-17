b3a00742d5e2973e31debf06b5da76772f6f7d33
Alexandru Morti <morti_alexandru@yahoo.com>
2015-11-17 09:53:46 +0000

    Created Project

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* ● Create a console application which outputs a random number between 1 and 6
+               ● Change the program so it asks the user how many dice to roll and then outputs:
+                        ● The individual value of each dice
+                        ● The number of dice rolled
+                        ● The total value of all the dice summed up
+               ● Create a class which represents a ‘die’ and restructure the program in 2 to use this
+               ● Create a child class of your ‘die’ which represents the ‘pigs’ in Pass the Pigs
+               ● Update your program so that running it simulates the throwing/rolling of 2 ‘Pigs’
+               ● Add code to calculate the score of a throw as in 5, based on the rules in WikiPedia
+               ● Update your program so that it can run a simulation of a ‘game’ of Pass the pigs with one computer and one human player
+                       ● To start with, consider a ‘game’ to be 5 throws and whoever has the highest score wins
+                       ● Review the rules and make notes here on any other considerations to be added to create the full game rules
+                */
+
+
+
+
+
+            //   ● Create a console application which outputs a random number between 1 and 6
+
+
+
+
+
+
+        }
+    }
+}
---
+++ PassThePigs/Module1.vb
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+Namespace GuessANumber
+{
+    Class Program
+    {
+        static void Main(string[] args)
+        {
+            /*●	Write a program which picks a random number between 1 and 100. Ask the user to guess the number. Tell them when they get it right (and then exit)
+              ●	Instead of exiting, ask the user if they would like to play again
+              ●	Give the user a hint - “Higher!” or “Lower!” depending on how close their guess was
+              ●	If the user is close (within 5 or 10 - you decide), then give them more help - “Almost! But Lower!”, etc
+              ●	Keep track of the number of guesses the user has to make and tell them how many it took at the end
+              ●	Track how long (in seconds or minutes) it takes the user to guess correctly and let them know when they get it right
+              ●	Allow the user to set the upper and lower limits when they start the game (wrap your logic in a class, so that you pass these values in when starting up)
+              ●	Write the same program, but the other way around! Make a program which will try to guess a number you are thinking of. Make it react to feedback such as “lower, higher”, etc!
+             */
+
+
+
+
+            //*●	Write a program which picks a random number between 1 and 100. Ask the user to guess the number. Tell them when they get it right (and then exit)//
+
+
+
+
+            // 1.Picks a random number between 1 and 100.//
+
+            bool userAnswer = false;
+
+            string userChoice;
+
+            Random random = new Random();
+            int numberToGuess = random.Next(1) + 1;
+            int userGuess = 0;
+
+
+            while (userGuess != numberToGuess)
+
+            {
+
+
+
+
+                // 2. Ask the user to guess the number.
+
+
+
+                Console.Write("Enter your guess: ");
+                int.TryParse(Console.ReadLine(), out userGuess);
+
+
+
+
+                // 3. Tell them when they get it right (and then exit).
+
+                // if wrong print - wrong ask again
+                if (userGuess > numberToGuess)
+                {
+                    Console.WriteLine("{0} is too high!", userGuess);
+                }
+
+
+                 //if right - congratulate
+                else if (userGuess < numberToGuess)
+                {
+                    Console.WriteLine("{0} is too low!", userGuess);
+                }
+
+
+                else
+                {
+
+                    //  ●	Instead of exiting, ask the user if they would like to play again
+
+                    //ask the user if they would like to play again
+
+                    Console.WriteLine("{0} is right! Do you want to start again?(y/n).", userGuess);
+
+
+                    userChoice = Console.ReadLine();
+
+
+                    //if no exit
+
+
+                    if (userChoice == "n")
+
+                    {
+                        Console.WriteLine("Thanks for playing!");
+
+                    }
+
+                         //if yes play again
+
+
+
+                    else if (userChoice == "y")
+                    {
+                        userAnswer = false;
+                    }
+
+
+                }
+
+
+            }
+
+            Console.ReadKey();
+
+        }
+        }
+    }
+
