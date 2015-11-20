7bcba52707b8c507a8e014644b75ae2ff470c02d
Alexandru Morti <morti_alexandru@yahoo.com>
2015-11-20 08:45:02 +0000

    Added two classes to the program Die and Pigs

---
+++ ConsoleApplication1/Die.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//● Create a class which represents a ‘die’ and restructure the program in 2 to use this
+
+namespace ConsoleApplication1
+{
+    class Die
+    {
+
+
+        public Die()
+        { 
+
+        }
+
+
+        public int roll()
+        {
+            Random roll = new Random();
+            int rollDice;
+            rollDice = roll.Next(1, 7);
+  
+            return rollDice;
+        }
+
+        public int roll2()
+        {
+
+        }
+
+    }
+}
---
+++ ConsoleApplication1/Pigs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//● Create a child class of your ‘die’ which represents the ‘pigs’ in Pass the Pigs
+
+
+namespace ConsoleApplication1
+{
+    class Pigs : Die
+    {
+        public Pigs()
+        { 
+            
+        }
+
+
+    }
+}
