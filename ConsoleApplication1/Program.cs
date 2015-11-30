b6f8efbf6edd27c10d2184682aa87eb8caa5d011
Matt Chatterley <matt@code.je>
2015-11-30 11:41:21 +0000

    Add comments/notes on implementing scoring

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -109,6 +109,12 @@ namespace ConsoleApplication1
 
             Console.Out.WriteLine("Total score for this round is " + " " + (score) + " " + "points");
 
+            // 13. Once scoring is calculating correctly for a single round, wrap the code above (starting from 'roll'?) in a loop to test it
+
+            // 14. How might this be extended to two players taking turns?
+
+            // 15. In reality, players can have more than one roll (hence pig out / makin bacon rules) per turn - how can we do this?
+
             //do {
             //     int i = 0;
             //     while (i <= 1)
@@ -131,6 +137,44 @@ namespace ConsoleApplication1
 
         private static int calculateScore(Pigs pig1, Pigs pig2)
         {
+            /*
+             * Single pig
+                The pig is lying on its side - 0 Points
+                Razorback - The pig is lying on its back - 5 Points
+                Trotter - The pig is standing upright - 5 Points
+                Snouter - The pig is leaning on its snout - 10 Points
+                Leaning Jowler - The pig is resting on its snout and ear - 15 Points
+
+             * Both pigs
+                Sider - The pigs are on their sides, either both with the spot facing upward or both with the spot facing downward - 1 Point
+                Double Razorback - The pigs are both lying on their backs - 20 Points
+                Double Trotter - The pigs are both standing upright - 20 Points
+                Double Snouter - The pigs are both leaning on their snouts - 40 Points
+                Double Leaning Jowler - The pigs are both resting between snouts and ears - 60 Points
+                Mixed Combo - A combination not mentioned above is the sum of the single pigs' scores
+                Pig Out - If both pigs are lying on their sides, one with the spot facing upwards and one with the spot facing downwards the score for that turn is reset to 0 and the turn changes to the next player
+                Makin' Bacon (or Oinker) - If both pigs are touching in any position,[2] then the total score is reset to 0 and the turn changes to the next player
+                Piggyback - If one pig lands completely resting on top of the other, then the player is out of the game. (According to the scoring rules on the front of the game package such a result is impossible.)
+             */
+
+            // 1. Check if the throw matches any combinations
+            
+            // 2. Ignore 'mixed combo' as this is just adding up the single scores
+            // 3. Makin' bacon / Piggyback are a special case to come back to later (once probabilities are introduced)
+            // 4. Pig Out - we may need to introduce the notion of 'which side' the pig is on, so come back to later
+
+            // 5. This leaves the 'simple' combinations where the pigs are showing the same value
+            // 6. This should be easy to work out and look up in a score dictionary (create dictionaries at the top of this method for now)
+
+            // 7. If no combination is matched, the score for the throw is the sum of the individual scores (already in dictionary in pig class)
+
+            // 8. return the total score
+
+            // 9. Come back to 'pig out' - add a 6th 'side' to pig which is 'on side, with spot' (current 'on side' becomes 'on side, no spot'
+            // 10. Bearing in mind the above, implement a case to catch 'pig out' in this method and return 0
+            
+            // 11. Create a class called 'Score' and move this method to it as a static method
+            // 12. We can use this class in the future (an instance) to track the scores for a whole game
             return 0;
         }
     }
